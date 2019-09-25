216914170e076ea1586dbc39b438a42dae82d5b6
list1331 <list1331@listxa0.local>
2019-09-26 01:31:07 +0900

    Add some functions for the permutations

--- number/number.lisp
+++ number/number.lisp
@@ -250,6 +250,41 @@
 		       (tail (cdr xs)))
 		   (g (f lst head) tail)))))
     (g '() xs)))
+
+;; decomp-permutation
+;; take permutation  and decomp it
+;; your permutation must be look like this
+;; '( '(1 . 2) '(2 . 1) '(3 . 3))
+
+(defun decomp-permutation (lst)
+  (let ((permu lst))
+    (labels ((f (head p)
+	       (let ((flag (assoc head p)))
+		 (if flag (cons head (f (cdr flag)
+					(setq permu (remove flag permu))))
+		     '())))
+	     (g (head)
+	       (if (null permu) '()
+		   (cons (f head permu) (g (car (car permu)))))))
+      (g 1))))
+
+;; sgn function for permutation
+
+(defun sgn-permu (permu)
+  (labels ((f (lst)
+	     (if (null lst) 1
+		 (let ((head (car lst))
+		       (tail (cdr lst)))
+		   (if (evenp (length head))
+		       (* -1 (f tail))
+		       (* 1 (f tail)))))))
+    (f (decomp-permutation permu))))
+	     
+	       
+	     
+  
+
+
 	     
     
 		   
